1b570c5c54d2a7f6a00ba1563599176dd8700477
Poverennov Mikhail <130486332+Poverennov-Mikhail@users.noreply.github.com>
2026-05-10 11:27:36 +0000

    test5

--- ts-from-zero/01-variables.ts
+++ ts-from-zero/01-variables.ts
@@ -1,19 +1,3 @@
-// ======================================
-// 1. Переменные и типы данных
-// ======================================
-
-let age: number = 30;
-const firstName: string = "Михаил";
-let isLogged: boolean = true;
-let price: number = 199.99;
-
-let lastName = "Повереннов";   // TypeScript сам определяет тип
-let isActive = false;
-
-console.log("=== Переменные ===");
-console.log("Имя:", firstName);
-console.log("Фамилия:", lastName);
-console.log("Возраст:", age);
-console.log("Цена:", price);
-console.log("Залогинен:", isLogged);
-console.log("Активен:", isActive);
+console.log("Привет! Это работает!");
+console.log("TypeScript запущен успешно");
+console.log("Мой первый скрипт");
